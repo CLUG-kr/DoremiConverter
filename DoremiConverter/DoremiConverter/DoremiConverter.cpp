6172f0061f526f1f0daa3af8a5ee1896ac3c6181
tyg04308 <tyg04308@naver.com>
2019-01-16 19:40:37 +0900

    OpenCv Init

---
+++ DoremiConverter/DoremiConverter/DoremiConverter.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main()
+{
+	std::cout << "OpenCV Version : " << CV_VERSION << std::endl;
+	cv::Mat img;
+	cv::namedWindow("EXAMPLE01", CV_WINDOW_AUTOSIZE);
+
+	img = cv::imread("kt.png", CV_LOAD_IMAGE_COLOR);
+	if (img.empty())
+	{
+		std::cout << "[!] You can NOT see the cat!" << std::endl;
+		return -1;
+	}
+	cv::imshow("EXAMPLE01", img);
+	cv::waitKey(0);
+	cv::destroyWindow("EXAMPLE01");
+	return 0;
+}
